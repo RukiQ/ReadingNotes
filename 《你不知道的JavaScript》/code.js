8192a7919923970bbe0dd993e3e0f58132ea1625
QianLu(营销事业部_技术部) <qianlu@sogou-inc.com>
2016-07-12 19:06:16 +0800

    0712

--- 《你不知道的JavaScript》/code.js
+++ 《你不知道的JavaScript》/code.js
@@ -1,52 +1,21 @@
-var MyModules = (function Manager() {
-  var modules = {};
+window.onload = function() {
+  function foo(num) {
+    console.log("foo:" + num);
 
-  function define(name, deps, impl) {
-    for (var i=0; i<deps.length; i++) {
-      deps[i] = modules[deps[i]];
+    this.count++;
+    console.log(this);
+  }
+
+  count = 0;
+
+  var i; 
+  for (var i=0; i<10; i++) {
+    if (i>5) {
+      foo(i);
     }
-    modules[name] = impl.apply(impl, deps);
   }
 
-  function get(name) {
-    return modules[name];
-  }
-
-  return {
-    define: define,
-    get: get
-  };
-})();
+  console.log(window.count);
+}
 
 
-// 定义模块
-MyModules.define('bar', [], function() {
-  function hello(who) {
-    return 'Let me introduce: ' + who;
-  }
-
-  return {
-    hello: hello
-  };
-});
-
-MyModules.define('foo', ['bar'], function() {
-  var hungry = 'hippo';
-
-  function awesome() {
-    console.log(bar.hello(hungry).toUpperCase());
-  }
-
-  return {
-    awesome: awesome
-  };
-});
-
-var bar = MyModules.get('bar');
-var foo = MyModules.get('foo');
-
-console.log(
-  bar.hello('hippo')
-);  // Let me introduce: hippo
-
-foo.awesome();  // LET ME INTRODUCE: HIPPO
---
+++ 《你不知道的JavaScript》/part2-this和对象原型.md
@@ -0,0 +1,13 @@
+### <p style="background:orange;">第1章 关于this</p>
+
+
+### <p style="background:orange;">第2章 this全面解析</p>
+
+
+### <p style="background:orange;">第3章 对象</p>
+
+### <p style="background:orange;">第4章 混合对象“类”</p>
+
+### <p style="background:orange;">第5章 原型</p>
+
+### <p style="background:orange;">第6章 行为委托</p>
