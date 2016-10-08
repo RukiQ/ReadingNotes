3973e99dee061a86b3b93d382488b20d47e8f68e
Ruth92 <Ruki_yuluxuan@126.com>
2016-10-08 11:38:11 +0800

    c6 modify

--- js-pattern/c6-代码复用模式.js
+++ js-pattern/c6-代码复用模式.js
@@ -134,6 +134,63 @@ var child2 = Object.create(parent, {
 child2.hasOwnProperty('age'); // true
 
 
+//========================== Klass ==========================
+var klass = function(Parent, props) {
+  var Child, F, i;
+
+  // 1. 新构造函数
+  Child = function() {
+    if (Child.uber && Child.uber.hasOwnProperty("__construct")) {
+      Child.uber.__construct.apply(this, arguments);
+    }
+    if (Child.prototype.hasOwnProperty("__construct")) {
+      Child.prototype.__construct.apply(this, arguments);
+    }
+  };
+
+  // 2. 继承
+  Parent = Parent || Object;
+  F = function() {};
+  F.prototype = Parent.prototype;
+  Child.prototype = new F();
+  Child.uber = Parent.prototype;
+  Child.prototype.constructor = Child;
+
+  // 3. 添加实现方法
+  for (i in props) {
+    if (props.hasOwnProperty) {
+      Child.prototype[i] = props[i];
+    }
+  }
+
+  // 返回该 'class'
+  return Child;
+}
+
+// 测试
+var Man = klass(null, {
+  __construct: function(what) {
+    console.log("Man's constructor");
+    this.name = what;
+  },
+  getName: function() {
+    return this.name;
+  }
+});
+
+var SuperMan = klass(Man, {
+  __construct: function(what) {
+    console.log("SuperMan's constructor");
+  },
+  getName: function() {
+    var name = SuperMan.uber.getName.call(this);
+    return "I am " + name;
+  }
+});
+
+var clark = new SuperMan('Clark Kent');
+clark.getName();  // 'I am Clark Kent'
+
 //========================== 7.通过复制属性实现继承 ==========================
 /**
  * 浅复制
